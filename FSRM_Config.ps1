248b49de16877cb02036160b72e242cbad1c5437
Alexander Reynolds <areynolds@etmcorad.com>
2016-09-25 19:05:45 -0400

    Fixed FileGroupUpdater script

--- FSRM_Config.ps1
+++ FSRM_Config.ps1
@@ -125,9 +125,15 @@ $Update_Script = @"
 `$date = Get-Date -Format dd-MM-yyyy
 `$FilePatterns = ((Invoke-WebRequest -Uri "https://fsrm.experiant.ca/api/v1/combined").content | ConvertFrom-Json | ForEach-Object {`$_.filters}) 
 `$OldFilePatterns = (Get-FSRMFileGroup -Name "Ransomware File Groups" ).IncludePattern
-`$Compare = Compare-Object -ReferenceObject `$OldFilePatterns -DifferenceObject `$FilePatterns | Out-File -filepath `$FSRM_Log_Path\Ransomware_File_Groups_`$date.txt
+`$Compare = Compare-Object -ReferenceObject `$OldFilePatterns -DifferenceObject `$FilePatterns
+if (`$Compare -eq `$null) {
+    `$body = "No new ransomware file extensions added this week."
+} else {
+    `$body = `$Compare | Out-String
+}
+`$Compare| Out-File -filepath `$FSRM_Log_Path\Ransomware_File_Groups_`$date.txt
 
-`Send-MailMessage -Body `$Compare -From `$from_email -To `$admin_email -Subject "Ransomware File Group Updated" -SmtpServer `$smtp_server
+`Send-MailMessage -Body `$Body -From `$from_email -To `$admin_email -Subject "Ransomware File Group Updated" -SmtpServer `$smtp_server
 `Set-FSRMFileGroup -Name "Ransomware File Groups" -IncludePattern `$FilePatterns
 
 "@
